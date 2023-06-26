538ce412640deadeed63e63126823d45879f5bd5
VladWithCode <vladwithcode@gmail.com>
2023-06-25 20:49:05 -0700

    [Add] Index page

---
+++ src/app/(Landing)/layout.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React, { useState } from 'react';
+import BackTopBtn from '../_components/BackTopBtn';
+import ObservableBar from '../_components/ObservableBar';
+import NavBar from './_components/NavBar';
+import './style.css';
+
+function Layout({ children }: { children: React.ReactNode }) {
+	const [isAtTop, setIsAtTop] = useState(false);
+
+	return (
+		<div className="relative z-0 h-full">
+			<ObservableBar setIsIntersecting={setIsAtTop} />
+			<div className="sticky top-0 left-1/2 right-1/2 z-40">
+				<NavBar />
+			</div>
+			{children}
+			<div className="fixed bottom-12 right-8 z-40">
+				<BackTopBtn isAtTop={isAtTop} />
+			</div>
+		</div>
+	);
+}
+
+export default Layout;
