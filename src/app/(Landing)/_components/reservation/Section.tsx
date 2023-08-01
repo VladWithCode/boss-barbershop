2e0721df54afd7739838a9923a814ac18b4cce3c
vladwithcode <vladwithcode@gmail.com>
2023-07-31 22:53:40 -0600

    [Change] Step layout

--- src/app/(Landing)/_components/reservation/Section.tsx
+++ src/app/(Landing)/_components/reservation/Section.tsx
@@ -1,29 +1,34 @@
-"use client";
-import React, { useState } from "react";
-import Calendar from "./Calendar";
+'use client';
+import React, { useState } from 'react';
+import Calendar from './Calendar';
+import CustomerData from './CustomerData';
+import Confirmation from './Confirmation';
 
-const CitaStepComponents = [
-  <Calendar key={0} />,
-
-]
+const STEP_INSTRUCTIONS = [
+  'Para comenzar selecciona una fecha disponible en el calendario y a continuación la hora en que te gustaria ser atendido',
+  'Ahora introduce tus datos para agendar la cita',
+  'Para finalizar revisa que la información sea correcta y confirma tu cita.',
+];
 
 function Section() {
   const [step, setStep] = useState(0);
-  
+
   return (
     <section className="text-main-dark bg-zinc-300 px-5">
       <h1 className="text-xl font-semibold uppercase text-center py-5">
         Agenda tu cita
       </h1>
       <p className="text-md font-normal text-justify w-[32ch] mx-auto">
-        Para comenzar selecciona una fecha disponible en el calendario y a
-        continuación la hora en que te gustaria ser atendido
+        {STEP_INSTRUCTIONS[step]}
       </p>
       {/* Calendar */}
-      <Calendar />
-      {
-        
-      }
+      {step === 0 ? (
+        <Calendar setStep={setStep} />
+      ) : step === 1 ? (
+        <CustomerData setStep={setStep} />
+      ) : step === 2 ? (
+        <Confirmation setStep={setStep} />
+      ) : null}
     </section>
   );
 }
